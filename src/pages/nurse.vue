a74b07b36574de64f1027f46eefd2480a9f7436e
Emilly <emymonteiro.sweetie@gmail.com>
2022-01-26 14:39:32 -0400

    registerPatient

---
+++ src/pages/nurse.vue
@@ -0,0 +1,80 @@
+<template>
+  <h1 class="text-gray-400 font-bold pt-10 p-5 text-2em">
+    Registrar Enfermeiro
+  </h1>
+  <form class="flex flex-col text-left w-[80%] pb-5 mt-5 space-y-5">
+    <!-- <div class="flex w-full space-x-10">
+      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Paciente
+      </button>
+      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        Enfermeiro
+      </button>
+    </div> -->
+    <div>
+      <label class="block text-gray-700 text-sm ml-1 font-bold mb-2" for="username">
+        Digite o Nome
+      </label>
+      <input
+        id="username"
+        type="text"
+        placeholder="Nome"
+        class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3"
+      >
+      <p class="text-red-500 ml-1 mt-2 text-xs italic">
+        Por favor insira um nome.
+      </p>
+    </div>
+    <div>
+      <label class="block text-gray-700 text-sm ml-1 font-bold mb-2" for="cpf">
+        Digite o CPF
+      </label>
+      <input
+        id="cpf"
+        v-maska="'###.###.###-##'"
+        type="text"
+        placeholder="CPF"
+        class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3"
+      >
+      <p class="text-red-500 ml-1 mt-2 text-xs italic">
+        Por favor insira um CPF.
+      </p>
+    </div>
+    <!-- Senha para o Enfermeiro -->
+    <div>
+      <label class="block text-gray-700 text-sm ml-1 font-bold mb-2" for="password">
+        Digite uma senha
+      </label>
+      <input
+        id="password"
+        type="text"
+        placeholder="Senha"
+        class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3"
+      >
+      <p class="text-red-500 ml-1 mt-2 text-xs italic">
+        Por favor insira uma senha.
+      </p>
+    </div>
+    {{ test }}
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded focus:outline-none focus:shadow-outline" type="button">
+      Registrar
+    </button>
+  </form>
+</template>
+
+<script lang="ts">
+
+import { maska } from 'maska'
+
+export default {
+  directives: {
+    maska,
+  },
+  data() {
+    return {
+      test: 'hello',
+    }
+  },
+
+}
+</script>
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,27 +1,20 @@
 <template>
-  <h1 class="text-gray-400 font-bold p-5 text-2em">
-    Registro
+  <h1 class="text-gray-400 font-bold pt-10 p-5 text-2em">
+    Registrar Paciente
   </h1>
   <form class="flex flex-col text-left w-[80%] pb-5 mt-5 space-y-5">
-    <div class="flex w-full space-x-10">
-      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Paciente
-      </button>
-      <button class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Enfermeiro
-      </button>
-    </div>
     <div>
       <label class="block text-gray-700 text-sm ml-1 font-bold mb-2" for="username">
         Digite o Nome
       </label>
       <input
         id="username"
+        v-model="inputs.name"
         type="text"
         placeholder="Nome"
         class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3"
       >
-      <p class="text-red-500 ml-1 mt-2 text-xs italic">
+      <p v-if="empty.name" class="text-red-500 ml-1 mt-2 text-xs italic">
         Por favor insira um nome.
       </p>
     </div>
@@ -31,37 +24,27 @@
       </label>
       <input
         id="cpf"
+        v-model="inputs.cpf"
         v-maska="'###.###.###-##'"
         type="text"
         placeholder="CPF"
         class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3"
       >
-      <p class="text-red-500 ml-1 mt-2 text-xs italic">
+      <p v-if="empty.cpf" class="text-red-500 ml-1 mt-2 text-xs italic">
         Por favor insira um CPF.
       </p>
-    </div>
-    <!-- Senha para o Enfermeiro -->
-    <!-- <div>
-      <label class="block text-gray-700 text-sm ml-1 font-bold mb-2" for="password">
-        Digite sua senha
-      </label>
-      <input
-        id="password"
-        type="text"
-        placeholder="Senha"
-        class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3"
-      >
-      <p class="text-red-500 ml-1 mt-2 text-xs italic">
-        Por favor insira sua senha.
+      <p v-if="!validCPF" class="text-red-500 ml-1 mt-2 text-xs italic">
+        Numero de CPF inválido.
       </p>
-    </div> -->
+    </div>
+
     <!-- Campos para o Paciente -->
     <div class="flex space-x-5 w-full overflow-hidden">
       <div class="w-[60%]">
         <label class="block text-gray-700 text-sm ml-1 font-bold mb-2" for="date">
           Data de Nascimento
         </label>
-        <input id="date" class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3" type="date">
+        <input id="date" v-model="inputs.date" class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3" type="date">
       </div>
       <div class="w-[30%]">
         <label class="block text-gray-700 text-sm ml-1 font-bold mb-2" for="weight">
@@ -69,6 +52,7 @@
         </label>
         <input
           id="weight"
+          v-model="inputs.weight"
           v-maska="['#','##','###']"
           class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3"
           type="text"
@@ -80,30 +64,112 @@
         </label>
         <input
           id="height"
+          v-model="inputs.height"
           v-maska="'#.##'"
           class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3"
           type="text"
         >
       </div>
     </div>
-    <label class="block text-gray-700 text-sm ml-1 font-bold mb-2">
-      Estado
-    </label>
-    <select class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3">
-      <option>Estado</option>
-    </select>
-    <!--  -->
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded focus:outline-none focus:shadow-outline" type="button">
+    <div>
+      <label class="block text-gray-700 text-sm ml-1 font-bold mb-2">
+        Estado
+      </label>
+      <select v-model="inputs.state" class="appearance-none border rounded outline-none text-gray-500 leading-tight shadow w-full p-3">
+        <option v-for="item in store" :key="item.id">
+          {{ item.sigla }}
+        </option>
+      </select>
+      <p v-if="empty.state" class="text-red-500 ml-1 mt-2 text-xs italic">
+        Por favor selecione um estado.
+      </p>
+    </div>
+
+    <p v-if="errorMSG !== ''" class="text-center text-red-500 ml-1 mt-2 font-bold">
+      {{ errorMSG }}
+    </p>
+
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold p-4 rounded focus:outline-none focus:shadow-outline" type="button" @click="handleSubmit">
       Registrar
     </button>
   </form>
 </template>
 
-<script lang="ts">
-
+<script>
 import { maska } from 'maska'
+import axios from 'axios'
 
 export default {
-  directives: { maska },
+  directives: {
+    maska,
+  },
+  data() {
+    return {
+      inputs: {
+        name: '',
+        cpf: '',
+        date: '',
+        weight: '',
+        height: '',
+        state: '',
+      },
+      validCPF: true,
+      errorMSG: '',
+      empty: {
+        name: false,
+        cpf: false,
+        state: false,
+      },
+      store: null,
+    }
+  },
+  async created() {
+  // GET request using fetch with async/await
+    const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
+    const data = await response.json()
+    this.store = data
+  },
+  methods: {
+    async handleSubmit() {
+      await this.emptyRules()
+      for (const each in this.empty)
+        if (this.empty[each]) return
+
+      if (this.inputs.cpf.length < 14) {
+        this.validCPF = false
+        return
+      }
+
+      const data = {}
+      for (const each in this.inputs)
+        data[each] = this.inputs[each]
+
+      const res = await axios.get('patients')
+      if (res.data.some(e => e.cpf === this.inputs.cpf)) {
+        this.errorMSG = 'Já existe um usuário cadastrado com esse CPF.'
+        return
+      }
+
+      axios.post('patients', data)
+        .then((res) => {
+          this.resetForm(true)
+        }).catch(() => {
+          this.errorMSG = 'Ocorreu algum erro inexperado.'
+          this.resetForm()
+        })
+    },
+    emptyRules() {
+      for (const each in this.empty)
+        this.empty[each] = this.inputs[each] === ''
+    },
+
+    resetForm(arg) {
+      for (const each in this.inputs)
+        this.inputs[each] = ''
+
+      this.errorMSG = arg ? '' : this.errorMSG
+      this.validCPF = true
+    },
+  },
 }
 </script>
